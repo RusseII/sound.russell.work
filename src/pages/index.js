cb3be7cb4c4c8a63aa2406da3c1a736b91f0870e
russell <russell@deephire.com>
2020-01-03 19:35:53 -0600

    background noise

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import Sound from 'react-sound';
+
+const MyComponentWithSound = () => (
+  <Sound
+      url="https://ia600803.us.archive.org/24/items/RainSounds10HoursTheSoundOfRainMeditationAutogencTrainingDeepSleepRelaxingSounds/Rain%20Sounds%2010%20HoursThe%20Sound%20of%20Rain%20MeditationAutogenc%20Training%20Deep%20SleepRelaxing%20Sounds.mp3"
+      playStatus={Sound.status.PLAYING}
+      playFromPosition={500}
+      loop
+    />
+)
+
+export default MyComponentWithSound
