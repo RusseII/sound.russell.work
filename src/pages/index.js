6bf79f01f00d65a3be2ce046d94f3b9f799fc1f9
russell <russell@deephire.com>
2020-01-04 02:47:17 -0600

    fix cap

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import SoundPlayer from '../components/SoundPlayer'
-import Leaderboard from '../components/LeaderBoard'
+import Leaderboard from '../components/Leaderboard'
 
 
 const BackgroundNoise = () => {
