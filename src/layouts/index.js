5b622a68c73e60c484aaadeddb4ee39f084ffccc
russell <russell@deephire.com>
2020-01-03 20:37:54 -0600

    add autoplay hack

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,7 +3,6 @@ import styles from './index.css';
 function BasicLayout(props) {
   return (
       <div className={styles.normal}>
-      
       {props.children}
     </div>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,26 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 import Sound from 'react-sound';
 
-const MyComponentWithSound = () => (
-  <Sound
+
+
+const MyComponentWithSound = () => {
+  const [playCount, setPlayCount] = useState(0)
+  const [allowAutoPlayHack, setAllowAutoPlayHack] = useState(false)
+return ( <>
+  <Sound key={allowAutoPlayHack}
+      // host this somewhere else, or change for a better soundtrack.
+      // this file is 10h long and 600mb
       url="https://ia600803.us.archive.org/24/items/RainSounds10HoursTheSoundOfRainMeditationAutogencTrainingDeepSleepRelaxingSounds/Rain%20Sounds%2010%20HoursThe%20Sound%20of%20Rain%20MeditationAutogenc%20Training%20Deep%20SleepRelaxing%20Sounds.mp3"
       playStatus={Sound.status.PLAYING}
-      playFromPosition={500}
       loop
+      // onPlaying gets rendered every second, no clue why
+      onPlaying={() => setPlayCount(count => count + 1)}
     />
+    { playCount < 1 && <button onClick={() => setAllowAutoPlayHack(true)}>allow sound to autoplay</button>}
+   </>
 )
+
+}
 
 export default MyComponentWithSound
