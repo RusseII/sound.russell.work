b5b713f8ddc2fe8c19c287b817b5fd03f005ad29
russell <russell@deephire.com>
2020-03-21 16:31:13 -0500

    add spotify

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import SoundPlayer from '../components/SoundPlayer'
+import SpotifyPlayer from '../components/SpotifyPlayer'
+
 
 import Leaderboard from '../components/Leaderboard'
 
 
 const BackgroundNoise = () => {
 
-  return <SoundPlayer />
+  return <SpotifyPlayer />
 
 
 }
