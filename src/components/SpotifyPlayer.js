a1ce7f1e7d3be5b6f93eca1e568807deb1159418
russell <russell@deephire.com>
2020-03-21 19:05:16 -0500

    move to create-react app

--- src/components/SpotifyPlayer.js
+++ src/components/SpotifyPlayer.js
@@ -1,18 +1,18 @@
-
+import React from 'react';
 
 const Spot = () => (
     <>
-<iframe src="https://open.spotify.com/embed/playlist/0NCspsyf0OS4BsPgGhkQXM" width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
-<iframe src="https://open.spotify.com/embed/playlist/2X5BPknxexv0e7DdDKUj0C" width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
-<iframe src="https://open.spotify.com/embed/playlist/4GOnSXcPfwj1rWKcEIc3uu" width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
-<iframe src="https://open.spotify.com/embed/playlist/37i9dQZF1DWZeKCadgRdKQ" width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
-<iframe src="https://open.spotify.com/embed/playlist/37i9dQZF1DWXLeA8Omikj7" width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
-<iframe src="https://open.spotify.com/embed/playlist/37i9dQZF1DX6VdMW310YC7" width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
-<iframe src="https://open.spotify.com/embed/playlist/4ZB8LNvjTqxyUZPzHyYYJq" width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
-<iframe src="https://open.spotify.com/embed/playlist/505Q6knWabzhM6RLttF0zp" width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
-<iframe src="https://open.spotify.com/embed/playlist/4llsd1fg6Tafk0pl1c6esZ" width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
-<iframe src="https://open.spotify.com/embed/playlist/59rBMYukiDqbIOGsPgwagZ" width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+        <iframe src="https://open.spotify.com/embed/playlist/0NCspsyf0OS4BsPgGhkQXM" width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+        <iframe src="https://open.spotify.com/embed/playlist/2X5BPknxexv0e7DdDKUj0C" width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+        <iframe src="https://open.spotify.com/embed/playlist/4GOnSXcPfwj1rWKcEIc3uu" width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+        <iframe src="https://open.spotify.com/embed/playlist/37i9dQZF1DWZeKCadgRdKQ" width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+        <iframe src="https://open.spotify.com/embed/playlist/37i9dQZF1DWXLeA8Omikj7" width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+        <iframe src="https://open.spotify.com/embed/playlist/37i9dQZF1DX6VdMW310YC7" width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+        <iframe src="https://open.spotify.com/embed/playlist/4ZB8LNvjTqxyUZPzHyYYJq" width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+        <iframe src="https://open.spotify.com/embed/playlist/505Q6knWabzhM6RLttF0zp" width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+        <iframe src="https://open.spotify.com/embed/playlist/4llsd1fg6Tafk0pl1c6esZ" width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+        <iframe src="https://open.spotify.com/embed/playlist/59rBMYukiDqbIOGsPgwagZ" width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
 
-</>)
+    </>)
 
 export default Spot
