d1b70538a06f35112c46c1b6e35cf0712eda9676
russell <russell@deephire.com>
2020-05-18 13:37:31 -0500

    add discord status

--- src/App.js
+++ src/App.js
@@ -4,7 +4,12 @@ import SpotifyPlayer from './components/SpotifyPlayer'
 
 function App() {
   return (
+    <div style={{textAlign: 'center'}}><div>
     <SpotifyPlayer/>
+    </div>
+    <iframe src="https://discordapp.com/widget?id=544340658945654815&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0"></iframe>
+    </div>
+    
   );
 }
 
